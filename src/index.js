ef7ce9e04dea032312a81fe0a7382633c957497a
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-03-05 10:33:20 +0100

    deleting tasks implemented

--- src/index.js
+++ src/index.js
@@ -94,9 +94,6 @@ function renderTasklist(project) {
         const taskToAdd = `<div class="task-card" id="${i}"><p>${project.tasks[i].title}</p>
         <p>${project.tasks[i].description}</p>
         <p>${project.tasks[i].colorCode}</p>
-        <div class="task-icons">
-        <svg class="edit-task" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit Task</title><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12H20A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4V2M18.78,3C18.61,3 18.43,3.07 18.3,3.2L17.08,4.41L19.58,6.91L20.8,5.7C21.06,5.44 21.06,5 20.8,4.75L19.25,3.2C19.12,3.07 18.95,3 18.78,3M16.37,5.12L9,12.5V15H11.5L18.87,7.62L16.37,5.12Z" /></svg>
-        <svg class="delete-task" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Delete Task</title><path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4M16,10V17A1,1 0 0,1 15,18H9A1,1 0 0,1 8,17V10H16M13.5,6L14.5,7H17V9H7V7H9.5L10.5,6H13.5Z" /></svg>
         </div>
         </div>`;
         if (project.tasks[i].status === "To Do") {
@@ -188,6 +185,14 @@ submitTaskEditBtn.addEventListener("click", (e) => {
         description;
     projectList[currentProject].tasks[currentTaskIndex].status = status;
     projectList[currentProject].tasks[currentTaskIndex].colorCode = colorCode;
+    renderTasklist(projectList[currentProject]);
+    editTaskModal.style.display = "none";
+});
+
+const deleteTaskBtn = document.querySelector(".task-delete");
+deleteTaskBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    projectList[currentProject].tasks.splice(currentTaskIndex, 1);
     renderTasklist(projectList[currentProject]);
     editTaskModal.style.display = "none";
 });
